366281a1aba7d14abb236d7a54776b47e1728ff7
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-07-24 09:17:24 -0400

    Completed enhancer.js file.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+    return { 
+        ...item, 
+        enhancement: item.enhancement === 20 ? item.enhancement : item.enhancement + 1,
+        durability: item.durability
+        };
 }
 
 function fail(item) {
-  return { ...item };
+    return { 
+        ...item,
+        enhancement: item.enhancement >  16 ? item.enhancement - 1 : item.enhancement,
+        durability: item.enhancement < 15 ? item.durability - 5 : item.durability - 10
+        };
 }
 
 function repair(item) {
-  return { ...item };
+    return { 
+        ...item, 
+        durability: 100 
+        };
 }
 
 function get(item) {
